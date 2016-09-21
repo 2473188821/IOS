f08432fe8cc5bc21d29a52c831a58430baf52b82
Chenfy <2473188821@qq.com>
2016-09-21 11:42:51 +0800

    view frame category

---
+++ FunctionTest/FunctionTest/Category/frameExpand.h
@@ -0,0 +1,18 @@
+//
+//  frameExpand.h
+//  FunctionTest
+//
+//  Created by Chenfy on 16/9/21.
+//  Copyright © 2016年 Chenfy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface frameExpand : UIView
+
+@property(nonatomic,assign)CGFloat  x;
+@property(nonatomic,assign)CGFloat  y;
+@property(nonatomic,assign)CGFloat  width;
+@property(nonatomic,assign)CGFloat  height;
+
+@end
